fcbad1ef09862c26a7d70693c704bb7cc92ca406
Zainab1979 <117858087+Zainab1979@users.noreply.github.com>
2023-03-19 12:51:59 -0700

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -2,3 +2,12 @@ library(dplyr)
 mechacar <- read.csv(file="MechaCar_mpg.csv")
 model= lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mechacar)
 summary(model)
+Suspension <- read.csv(file="Suspension_Coil.csv")
+total_summary <- summarize(Suspension,Mean= mean(PSI, na.rm = TRUE),Median=median(PSI, na.rm = TRUE),Variance=var(PSI, na.rm = TRUE),SD=sd(PSI, na.rm = TRUE))
+print(total_summary)
+
+lot_summary<-Suspension  %>%
+group_by(Manufacturing_Lot) %>%
+  summarize(Mean = mean(PSI, na.rm = TRUE),Median=median(PSI, na.rm = TRUE),Variance=var(PSI, na.rm = TRUE),SD=sd(PSI, na.rm = TRUE))
+print(lot_summary)
+
